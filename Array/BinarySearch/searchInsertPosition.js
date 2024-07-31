f0164c5aa7837ab3ca924681c34a2c1ba8115b97
neutricoindia <166170548+neutricoindia@users.noreply.github.com>
2024-08-01 02:39:07 +0530

    updated

--- Array/BinarySearch/searchInsertPosition.js
+++ Array/BinarySearch/searchInsertPosition.js
@@ -1,15 +1,21 @@
-const searchInsertPosition = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
-  let ans = arr.length;
-  while (left < right) {
-    let mid = Math.floor((left + right) / 2);
-    if (arr[mid] >= target) {
-      ans = mid;
+var searchInsert = function (nums, target) {
+  var len = nums.length;
+  var left = 0;
+  var right = len - 1;
+  var mid = 0;
+
+  if (!len) return 0;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] > target) {
       right = mid - 1;
-    } else {
+    } else if (nums[mid] < target) {
       left = mid + 1;
+    } else {
+      return mid;
     }
   }
-  return ans;
+
+  return nums[mid] > target ? mid : mid + 1;
 };

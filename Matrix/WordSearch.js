b1a047d03da21134f43348db3b2c644352832410
neutricoindia <166170548+neutricoindia@users.noreply.github.com>
2024-08-08 01:01:46 +0530

    update

--- Matrix/WordSearch.js
+++ Matrix/WordSearch.js
@@ -1,45 +1,48 @@
-const wordSearch = (matrix, word) => {
-  let ROW = matrix.length;
-  let COL = matrix[0].length;
-  let length = word.length;
+const wordSearch = (board, word) => {
+  var ROW = board.length;
+  var COL = (board[0] || []).length;
+  var length = word.length;
+  var visited = null;
 
-  function dfs(row, col, i, visited) {
-    if (i === length) {
-      return true;
-    }
+  if (!ROW || !COL || !length) return false;
 
-    if (
-      row < 0 ||
-      col < 0 ||
-      row >= ROW ||
-      col >= COL ||
-      word[i] != matrix[row][col] ||
-      visited[row][col]
-    ) {
-      return false;
-    }
-    visited[row][col] = true;
-    let res =
-      dfs(row + 1, col, i + 1, visited) ||
-      dfs(row - 1, col, i + 1, visited) ||
-      dfs(row, col + 1, i + 1, visited) ||
-      dfs(row, col - 1, i + 1, visited);
-
-    if (res) {
-      visited[row][col] = false;
-    }
-    return res;
-  }
-
-  for (let i = 0; i < ROW; i++) {
-    for (let j = 0; j < COL; j++) {
+  for (var i = 0; i < ROW; i++) {
+    for (var j = 0; j < COL; j++) {
       visited = Array(ROW)
         .fill(0)
         .map((_) => Array(COL));
-      if (dfs(i, j, 0, visited)) return true;
+      if (helper(board, word, i, j, 0, visited)) return true;
     }
   }
+
   return false;
+};
+
+var helper = function (board, word, row, col, index, visited) {
+  if (index === word.length) return true;
+  if (
+    row < 0 ||
+    col < 0 ||
+    row >= board.length ||
+    col >= board[row].length ||
+    visited[row][col] ||
+    board[row][col] !== word[index]
+  )
+    return false;
+
+  var res = false;
+
+  visited[row][col] = true;
+
+  res =
+    helper(board, word, row - 1, col, index + 1, visited) ||
+    helper(board, word, row + 1, col, index + 1, visited) ||
+    helper(board, word, row, col - 1, index + 1, visited) ||
+    helper(board, word, row, col + 1, index + 1, visited);
+
+  if (!res) visited[row][col] = false;
+
+  return res;
 };
 
 console.log(
@@ -49,6 +52,6 @@ console.log(
       ["S", "F", "C", "S"],
       ["A", "D", "E", "E"],
     ],
-    "ABCB"
+    "ABCCS"
   )
 );
